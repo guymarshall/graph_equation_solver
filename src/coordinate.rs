79f5aae8323c1cd19e93a4765cead32eb46b4603
Guy Marshall <guymarshall9@gmail.com>
2025-06-08 22:47:30 +0100

    WIP: Initial commit

---
+++ src/coordinate.rs
@@ -0,0 +1,15 @@
+pub(crate) struct Coordinate {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Coordinate {
+    pub fn new(x: i32, y: i32) -> Self {
+        Coordinate { x, y }
+    }
+}
+impl std::fmt::Display for Coordinate {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
